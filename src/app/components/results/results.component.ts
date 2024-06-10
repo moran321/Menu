095de5cfc7c54700ceec9524c4ab9aba86f49a63
MoranAn <moran.ank@gmail.com>
2024-06-11 00:02:49 +0300

    add colors by voting volume

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -43,53 +43,16 @@ export class ResultsComponent implements OnInit {
       }
     );
   }
-  // fetchResults(menuId: number): void {
-  //   this.resSubs = this.menuService.getResults(menuId).pipe(first()).subscribe(
-  //     data => {
-  //       let votes =  data[menuId].map((dish: { id: number, name: string; votes: number; photo: string, description: string })=> 
-  //         ({ name: dish.name, votes: dish.votes, photo: dish.photo, description: dish.description }))
-  //         .sort((a: { votes: number; }, b: { votes: number; }) => b.votes - a.votes);
-  //       if (menuId === 1) {      
-  //       this.results_promoted = votes; 
-  //       }
-  //       if (menuId === 2) {
-  //         this.results_regular = votes;
-  //       }
-  //     },
-  //     error => {
-  //       console.error('Error:', error);
-  //     }
-  //   );
-  // }  
-  
-  // getPhoto(dish: number): string {
-  //   const photos: { [key: number]: string } = {
-  //     1: 'assets/dish1.jpg',
-  //     2: 'assets/dish2.jpg',
-  //     3: 'assets/dish3.jpg',
-  //     4: 'assets/dish4.jpg'    };
-  //   return photos[dish] || 'assets/default.jpg'; // Fallback to a default image if the dish is not found
-  // }
 
 
-  compareResults(): void {
-    this.comparison = this.results_promoted.map((item1, index) => {
-      const item2 = this.results_regular[index];
-      const totalVotes1 = this.results_promoted.reduce((acc, item) => acc + item.votes, 0);
-      const totalVotes2 = this.results_regular.reduce((acc, item) => acc + item.votes, 0);
-
-      const percentage1 = ((item1.votes / totalVotes1) * 100).toFixed(2);
-      const percentage2 = ((item2.votes / totalVotes2) * 100).toFixed(2);
-
-      return {
-        name1: item1.name,
-        votes1: item1.votes,
-        percentage1,
-        name2: item2.name,
-        votes2: item2.votes,
-        percentage2
-      };
-    });
+  getClassBasedOnVotes(votes: number): string {
+    if (votes >= 50) {
+      return 'votes-high';
+    } else if (votes >= 20) {
+      return 'votes-medium';
+    } else {
+      return 'votes-low';
+    }
   }
 
   ngOnDestroy(): void {
